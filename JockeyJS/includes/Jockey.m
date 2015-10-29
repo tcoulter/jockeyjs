8e19c060bc6744b8287f3d85320a654989a692c0
Leo <superartstudio@gmail.com>
2015-10-29 17:27:37 +1100

    test

--- JockeyJS/includes/Jockey.m
+++ JockeyJS/includes/Jockey.m
@@ -51,7 +51,8 @@
 
 + (void)on:(NSString*)type perform:(JockeyHandler)handler
 {
-    void (^ extended)(NSDictionary *payload, void (^ complete)()) = ^(NSDictionary *payload, void(^ complete)()) {
+	void (^ extended)(UIWebView *webView, NSDictionary *payload, void (^ complete)()) = ^(UIWebView *webView, NSDictionary *payload, void(^ complete)()) {
+    //void (^ extended)(NSDictionary *payload, void (^ complete)()) = ^(NSDictionary *payload, void(^ complete)()) {
         handler(payload);
         complete();
     };
